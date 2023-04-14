9e66865c6a8626dcc84635980a47797e0fe579fe
bunga <bungasuweca@gmail.com>
2023-04-14 10:54:57 +0800

    Add files via upload

--- GameOver.js
+++ GameOver.js
@@ -1,6 +1,79 @@
-import { Text } from "react-native";
+import { Image, Text, View, StyleSheet} from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../consts/colors";
 
 function GameOver() {
-    return <Text> Game Over !! </Text>
+    return ( 
+        <View style={styles.root}>
+        <Text style={styles.hurei}>ヾHooray!</Text> 
+        <Text style={styles.ovrtitle}>I Have Guessed Your Number! :P</Text>
+        <View style={styles.imgContainer}>
+            <Image style={styles.img} source={require('../img/pocacchoow.png')}/>
+        </View>
+        <View>
+            <Text style={styles.sum}>
+                Your phone needed <Text style={styles.highlight}> [X] </Text> round to guess the number{'-'} <Text style={styles.highlight}> [Y] </Text></Text>
+        </View>
+        
+    </View>);
 }
 export default GameOver;
+
+const  styles = StyleSheet.create({
+    root:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary1,
+    },
+    hurei: {
+        fontSize: 40,
+        paddingBottom: 10,
+        fontWeight:'bold',
+        color: 'white',
+        elevation: 80,
+        textShadowColor:'#585858',
+        textShadowOffset:{width: 2, height: 3},
+        textShadowRadius:4,
+    },
+    ovrtitle:{
+        fontSize: 20,
+        fontWeight: '900',
+        backgroundColor: Colors.primary2,
+        color: 'white',
+        textAlign: 'center',
+        borderWidth: 4,
+        borderRadius: 30,
+        padding: 9,
+        borderColor: Colors.primary2
+        
+    },
+
+    imgContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: '#ebfaf3',
+        overflow: 'hidden',
+        margin: 10,
+        backgroundColor: '#ebfaf3'
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+    },
+
+    sum:{
+        padding: 20,
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center'
+    },
+    highlight:{
+        fontSize: 23,
+        fontWeight: "bold",
+        color: Colors.primary3
+    },
+
+});
--- Main.js
+++ Main.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
 import Primarybtn from "../components/ui/Primarybtn";
 import Card from "../components/ui/card";
+import Instruction from "../components/ui/instruction";
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max-min)) + min;
@@ -53,10 +54,14 @@ function Main({userNumber, onGameOver}) {
             <Title>Tebakan aq ♪(^∇^* : </Title>
             <NumberContainer>{currentGuess}</NumberContainer>                
                 <Card>
-                    <Text>Lebih tinggi atw lebih kecil?</Text>
-                    <View>
-                        <Primarybtn onPress={nextGuessHandler.bind(this, 'lower')}> - </Primarybtn>
-                        <Primarybtn onPress={nextGuessHandler.bind(this, 'greater')}> + </Primarybtn>
+                    <Instruction style={styles.Instruction}>Lebih tinggi atw lebih kecil?</Instruction>
+                    <View style={styles.buttons}>
+                        <View style={styles.button}>
+                            <Primarybtn onPress={nextGuessHandler.bind(this, 'lower')}> - </Primarybtn>
+                        </View>
+                        <View style={styles.button}>
+                            <Primarybtn onPress={nextGuessHandler.bind(this, 'greater')}> + </Primarybtn>
+                        </View>
                     </View>
                 </Card>
             </View>
@@ -67,10 +72,25 @@ function Main({userNumber, onGameOver}) {
 export default Main;
 
 const styles = StyleSheet.create({
-    
     screen: {
         flex: 1,
         padding: 30,
     },
+
+    Instruction:{
+        fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: 12,
+        paddingTop: 2
+    },
+
+    button: {
+        flex: 1,
+    },
+    
+    buttons:{
+        flexDirection: 'row',
+    },
+
     
 })
